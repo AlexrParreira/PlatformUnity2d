6640f03d1aed58a639ebb5e68c6cfafbc25b659f
AlexrParreira <72107472+AlexrParreira@users.noreply.github.com>
2022-03-17 15:29:02 -0300

    Platform_inicial

---
+++ PlatformUnity2d/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    
+    [SerializeField] private Transform levelPart;
+    [SerializeField] private Transform levelPart_Start;
+
+   
+   private void Awake(){
+     Transform lastLevelTransform;
+     lastLevelTransform = SpawnLevelPart(levelPart_Start.Find("End_Position").position);
+    lastLevelTransform = SpawnLevelPart(lastLevelTransform.Find("End_Position").position);
+    lastLevelTransform = SpawnLevelPart(lastLevelTransform.Find("End_Position").position);
+    lastLevelTransform = SpawnLevelPart(lastLevelTransform.Find("End_Position").position);
+    lastLevelTransform = SpawnLevelPart(lastLevelTransform.Find("End_Position").position);
+    lastLevelTransform = SpawnLevelPart(lastLevelTransform.Find("End_Position").position);
+      
+      
+   }
+   
+   private Transform SpawnLevelPart(Vector3 spawnPosition){
+       Transform LevelPartTransform = Instantiate(levelPart, spawnPosition, Quaternion.identity);
+       return LevelPartTransform;
+   }
+}
